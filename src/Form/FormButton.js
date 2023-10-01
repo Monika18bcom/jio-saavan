591f4746fd76f586203d68f2016b47f91ca43f94
Monika <monika.kbcom18@gmail.com>
2023-10-01 20:16:03 +0530

    button format

--- src/Form/FormButton.js
+++ src/Form/FormButton.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function FormButton({style}) {
   return (
-    <div className='form-button' style={{backgroundColor: style.bg , color: style.color, textAlign: style.textAlign, height: style.height , width: style.width, borderRadius: style.borderRadius, padding:style.padding}}>{style.content}</div>
+    <div className='form-button' style={{backgroundColor: style.bg , color: style.color, textAlign: style.textAlign, height: style.height , width: style.width, borderRadius: style.borderRadius, padding:style.padding}}>
+      {style.content}
+    </div>
   )
 }
 
