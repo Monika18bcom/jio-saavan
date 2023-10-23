75ac94dc2d6075254d586c5a74f49b16d83ae5ad
Monika <monika.kbcom18@gmail.com>
2023-10-23 12:49:40 +0530

    responsiveness - go pro

--- src/NavBar/Table.js
+++ src/NavBar/Table.js
@@ -9,8 +9,8 @@ function Table({e}) {
   return (
     <div className='table-row-section'>
       <div className='data1'>{e.col1}</div>
-      <div className='data2' style={{fontSize: e.col2 === 'Free' && '18px'  , color: !e.col2 ? '#f6f6f6' : e.col2 === true && '#2bc5b4'}}>{!e.col2 ? <FaXmark /> : e.col2 === 'Free' ? e.col2 : <HiCheck />}</div>
-      <div className='data3' style={{borderRadius: (e.border === 'top' && '4px 4px 0 0') || (e.border === 'bottom' && '0 0 4px 4px') , fontSize: e.col3 ? '18px' : '25px' }}>{e.col3 ? e.col3 : <HiCheck /> }</div>
+      <div className='data2' style={{fontSize: e.col2 !== 'Free' && '25px'  , color: !e.col2 ? '#DFDFDF' : e.col2 === true && '#2bc5b4'}}>{!e.col2 ? <FaXmark /> : e.col2 === 'Free' ? e.col2 : <HiCheck />}</div>
+      <div className='data3' style={{borderRadius: (e.border === 'top' && '4px 4px 0 0') || (e.border === 'bottom' && '0 0 4px 4px') , fontSize: e.col3 !== 'Pro' && '25px' }}>{e.col3 ? e.col3 : <HiCheck /> }</div>
     </div>
   )
 }
