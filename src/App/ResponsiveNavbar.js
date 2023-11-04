48d093fbabcd37fa4ab6bc1aae84be7308cbc73a
Monika <monika.kbcom18@gmail.com>
2023-11-04 19:57:05 +0530

    resnav updated

--- src/App/ResponsiveNavbar.js
+++ src/App/ResponsiveNavbar.js
@@ -1,42 +1,63 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {IoIosSearch} from 'react-icons/io'
 import {GoPerson} from 'react-icons/go'
 import {GoHome} from 'react-icons/go'
 import {AiOutlineCompass} from 'react-icons/ai'
 import './ResponsiveNavbar.css'
+import { JiosaavnContext } from './App'
+import { useNavigate } from 'react-router-dom'
 
-// GrHomeRounded
-// GoHome
-// BsPerson
-// AiOutlineCompass
 
 function ResponsiveNavbar() {
     const profileImg = 'https://staticfe.saavn.com/web6/jioindw/dist/1696482270/_i/default_images/default-user-150x150.jpg'
 
+    const {userData} = useContext(JiosaavnContext)
+
+    const navigate = useNavigate()
+
   return (
     <ul className='responsive-navbar-container'>
-        <li className='home'>
+        <li className='home' onClick={()=>navigate('/')} >
             <GoHome className='icon' />
             <span>Home</span>
         </li>
-        <li className='search'>
+        <li className='search' onClick={()=>navigate('/')} >
             <IoIosSearch className='icon' />
             <span>Search</span>
         </li>
-        <li className='browse'>
+        <li className='browse' onClick={()=>navigate('/')} >
             <AiOutlineCompass className='icon' />
             <span>Browse</span>
         </li>
-        <li className='login'>
-            <GoPerson className='icon' />
-            <span>Login</span>
-        </li>
-        {/* <li className='my-music'>
-            <img src={profileImg} alt='Profile Image' ></img>
-            <span>My Music</span>
-        </li> */}
+
+        {
+            userData?.userDetails !== null ? 
+            <li className='my-music' onClick={()=>navigate('/')} >
+                <img src={profileImg} alt='Profile Image' ></img>
+                <span>My Music</span>
+            </li> :
+            <li className='login' onClick={()=>navigate('/login')} >
+                <GoPerson className='icon' />
+                <span>Login</span>
+            </li>
+
+
+            // <div className="nav-bar-user-profile" onClick={handleProfile} >
+            //     <img className="nav-bar-profile-img" src={profileImg} alt='Profile Image' ></img>
+            //     {profileSelected ? <RiArrowUpSLine className="arrow-icon" /> : <RiArrowDownSLine className="arrow-icon" />}
+            // </div> :
+            // <ul className="right-ul">
+            //     <li><NavLink className="navlink" to="/login">Log In</NavLink></li>
+            //     <li><NavLink className="navlink" to="/signup">Sign Up</NavLink></li>
+            // </ul>
+        }
+
+        
+        
     </ul>
   )
 }
 
 export default ResponsiveNavbar
+
+
